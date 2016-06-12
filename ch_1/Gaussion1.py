770c58e4afa02bb85af09a5934a18358c957a1b8
Kapil Saraswat <kapil-iitk@users.noreply.github.com>
2016-06-12 21:22:12 +0530

    Update Gaussion1.py
    please see equation 1.7
    s is present in denominator.
    Thank you.

--- ch_1/Gaussion1.py
+++ ch_1/Gaussion1.py
@@ -8,6 +8,9 @@ s = 2.0
 x = 1.0
 
 f = 1 / sqrt(2 * pi) * exp(-0.5 * ((x - m) / s) ** 2)
+## see equation 1.7; s is missing
+## correct one is 
+#f = 1 / (s*sqrt(2 * pi) * exp(-0.5 * ((x - m) / s) ** 2))
 print f
 
 """
